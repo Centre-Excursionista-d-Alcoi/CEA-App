6b86c4b8648d96c37342dea4455eedc46c6062c9
Arnau Mora <arnyminer.z@gmail.com>
2021-03-09 00:20:53 +0100

    :lipstick: Working on the app's design

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: [Locale('en', 'US')],
-        path: 'assets/translations', // <-- change the path of the translation files
-        fallbackLocale: Locale('en', 'US'),
-        child: MyApp()
-    ),
+        supportedLocales: [Locale('ca', 'ES')],
+        path: 'assets/translations',
+        // <-- change the path of the translation files
+        fallbackLocale: Locale('ca', 'ES'),
+        child: MyApp()),
   );
 }
 
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       locale: context.locale,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
       home: MainPage(),
     );
@@ -38,20 +38,39 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
+class NotificationItem {
+  NotificationItem({
+    this.title,
+    this.message,
+    this.icon,
+    this.isExpanded = false,
+  });
+
+  String title;
+  String message;
+  int icon;
+  bool isExpanded;
+}
+
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
-  static const List<Widget> _navigationItems = <Widget>[
-    Center(
-      child: Text("Notifications"),
+
+  List<NotificationItem> _notifications = [
+    NotificationItem(
+      icon: 1,
+      title: "Secció d'Escalada",
+      message: "Reunió el pròxim 23 de març de 2021.",
     ),
-    Center(
-      child: Text("Calendar"),
+    NotificationItem(
+      icon: 2,
+      title: "Secció d'Escalada",
+      message: "Paco Franco ha agafat una corda de 30 metres del magatzem.",
     ),
-    Center(
-      child: Text("Card"),
-    ),
-    Center(
-      child: Text("Settings"),
+    NotificationItem(
+      icon: 1,
+      title: "General",
+      message:
+          "Ha arribat la teua federativa. Per favor, passa pel CEA en horari d'oficina a recollir-la.",
     ),
   ];
 
@@ -64,7 +83,56 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _navigationItems[_selectedIndex]),
+      body: Container(
+          child: <Widget>[
+        SingleChildScrollView(
+          child: Container(
+            child: ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  _notifications[index].isExpanded = !isExpanded;
+                });
+              },
+              children:
+                  _notifications.map<ExpansionPanel>((NotificationItem item) {
+                return ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    print(item.icon);
+                    return ListTile(
+                      leading: <Icon>[
+                        Icon(Icons.close_rounded),
+                        Icon(Icons.people_rounded),
+                        Icon(Icons.book_rounded),
+                      ][item.icon == null ? 0 : item.icon],
+                      title: Text(item.title),
+                    );
+                  },
+                  body: ListTile(
+                    title: Text(item.message),
+                    trailing: Icon(Icons.delete_rounded),
+                    onTap: () {
+                      setState(() {
+                        _notifications
+                            .removeWhere((currentItem) => item == currentItem);
+                      });
+                    },
+                  ),
+                  isExpanded: item.isExpanded,
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+        Center(
+          child: Text("Calendar"),
+        ),
+        Center(
+          child: Text("Card"),
+        ),
+        Center(
+          child: Text("Settings"),
+        ),
+      ][_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -79,15 +147,51 @@ class _MainPageState extends State<MainPage> {
             icon: Icon(Icons.person_rounded),
             label: tr("item_card"),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings_rounded),
-            label: tr("item_settings"),
-          ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.lightGreen[600],
         unselectedItemColor: Colors.grey,
         onTap: _onItemTapped,
+      ),
+      appBar: AppBar(title: const Text('Centre Excursionista d\'Alcoi')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.lightGreen[500],
+              ),
+              child: Wrap(
+                direction: Axis.vertical,
+                children: [
+                  Text(
+                    'CEA App',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text('info_version').tr(),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.add_rounded),
+              title: Text('side_item_new_event').tr(),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.qr_code_scanner_rounded),
+              title: Text('side_item_scanner').tr(),
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.settings_rounded),
+              title: Text('side_item_settings').tr(),
+            ),
+          ],
+        ),
       ),
     );
   }
